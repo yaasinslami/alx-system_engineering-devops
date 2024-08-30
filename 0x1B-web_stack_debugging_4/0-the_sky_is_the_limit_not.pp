ffb7010c04d0de31eeb650fd408d46bf95e04c77
yaasinslami <yassinx4002@gmail.com>
2024-08-30 03:24:52 +0100

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # increases the upper limit and restart nginx
 exec { 'increase upper limit':
   provider => shell,
-  command  => 'sed -i s/15/1000000/ /etc/default/nginx'
+  command  => 'sed -i s/15/5000/ /etc/default/nginx'
 }
 exec { 'restart nginx server':
   provider => shell,
